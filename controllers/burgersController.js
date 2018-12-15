83dcbac50c157e1a6b6d4652d24204ab108dd606
Big Fudge <mkf1302@hotmail.com>
2018-12-14 22:13:09 -0500

    syntax needs updating but code runs

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/app/api/burgers", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   burger.create([
     "name", "sleepy"
   ], [
@@ -27,7 +27,7 @@ router.post("/app/api/burgers", function(req, res) {
   });
 });
 
-router.put("/app/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
