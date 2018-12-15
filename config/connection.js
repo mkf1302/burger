209beb35c4b5eaeba26e19f5ad169bf82e95fd42
Big Fudge <mkf1302@hotmail.com>
2018-12-15 13:09:33 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
+if (process.env.NODE_ENV==="production") {
     connction = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
