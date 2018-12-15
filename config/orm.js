9abcdd214d1dda7ef9a3a9b85fce40b09c66f37b
Big Fudge <mkf1302@hotmail.com>
2018-12-15 11:05:56 -0500

    heroku upload

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,6 @@
 // Import MySQL connection.
-var connection = require("../config/connection.js");
+// var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
 // Helper function for SQL syntax.
 function printQuestionMarks(num) { 
